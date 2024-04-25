db3153c48020cc662f3e92ccd2c318a711519b4f
jeffmakuto <hjeffmakuto@gmail.com>
2024-04-25 09:06:20 +0000

    Register user

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python3
 """ Flask app """
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from typing import Dict
+from auth import Auth
 
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
+AUTH = Auth()
 
 @app.route('/', methods=['GET'])
 def welcome() -> Dict[str, str]:
@@ -16,6 +18,26 @@ def welcome() -> Dict[str, str]:
         dict: A dictionary containing a welcome message.
     """
     return jsonify({"message": "Bienvenue"})
+
+@app.route('/users', methods=['POST'])
+def users(email: str, password: str) -> Dict[str, str]:
+    """
+    Register a new user.
+
+    Expects form data with 'email' and 'password' fields.
+
+    Returns:
+        str: A JSON string containing a success message
+             or an error message if registration fails.
+    """
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    try:
+        AUTH.register_user(email, password)
+        return jsonify({"email": email, "message": "user created"})
+    except Exception:
+        return jsonify({"message": "email already registered"}), 400
 
 
 if __name__ == "__main__":
